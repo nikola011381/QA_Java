49359f4e248b61516a0863966c5b284438bd739c
Nikola <37514211+nikola011381@users.noreply.github.com>
2022-12-17 19:55:46 +0100

    Vezba 5

---
+++ Vezba5.java
@@ -0,0 +1,8 @@
+public class Vezba5 {
+    public static void main(String[] args) {
+        System.out.println("Unesi svoje ime:");
+        String name = System.console().readLine();
+        System.out.println("Dobrodosao " + name  );
+
+    }
+    }
