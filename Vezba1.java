e9cc865b15949189db556ae7c777afa476497521
Nikola <37514211+nikola011381@users.noreply.github.com>
2022-12-17 18:56:10 +0100

    Hello world commit

--- Vezba1.java
+++ Vezba1.java
@@ -1,6 +1,24 @@
 public class Vezba1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+/**
+ *
+ */
+        System.out.println("Hello world !!"); // komentar
 
-         System.out.println("Hello world !!");
+        /*
+         komentar
+        */
+
+        /**
+         *
+         */
+
+        System.out.println(2 + 2);
+        ispisiText("cao");
     }
+
+    public static void ispisiText (String text  ) {
+        System.out.println(text);
+    }
+
 }
---
+++ Vezba2.java
@@ -0,0 +1,13 @@
+public class Vezba2 {
+    /**
+     * Sabiranje dva broja
+     * @param args
+     */
+
+ public static void main(String[] args) {
+ int operand1=2;
+ int operand2=3;
+ int sum=operand1 + operand2;
+ System.out.println(sum);
+ }
+ }
